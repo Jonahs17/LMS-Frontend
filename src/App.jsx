9b9d920d3130154f870f7a9309886ab3c7a4532a
Jonahs17 <jonahsgeorge17@gmail.com>
2024-06-28 02:59:36 +0530

    aboutus added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import { Route, Routes } from 'react-router-dom';
 
+import Aboutus from './pages/Aboutus';
 import Home from './pages/Home';
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<Aboutus/>}/>
+
     </Routes>
   )
 }
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -45,47 +45,57 @@ function HomeLayout({ children }){
                 </div>
                 <div className="drawer-side w-0">
                     <label htmlFor="my-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
-                        <li className="w-fit absolute right-2 z-50">
+                    <ul className='menu p-4 w-48 h-[100%] sm:w-80 bg-base-200 text-base-content relative'>
+                        <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24}/>
                             </button>
                         </li>
                         <li>
-                            <Link to='/'>Home</Link>
+                            <Link to="/"> Home </Link>
                         </li>
-                        { isLoggedIn && role === 'ADMIN' && (
+                        {isLoggedIn && role === "ADMIN" && (
                             <li>
                                 <Link to="/admin/dashboard">Admin Dashboard</Link>
                             </li>
-                        )}
+                        )
+
+                        }
+                        {isLoggedIn && role === "ADMIN" && (
+                            <li>
+                                <Link to="/course/create">Create Course</Link>
+                            </li>
+                        )
+
+                        }
                         <li>
-                            <Link to='/about'>About Us</Link>
+                            <Link to="/about"> About us </Link>
                         </li>
                         <li>
-                            <Link to='/contact'>Contact Us</Link>
+                            <Link to="/contact"> Contact us </Link>
                         </li>
                         <li>
-                            <Link to='/course'>All Courses</Link>
+                            <Link to="/courses"> All courses </Link>
                         </li>
+
                         {!isLoggedIn ? (
-                            <li className="relative bottom-4 w-full mt-8">
+                            <li className="absolute bottom-4 w-[90%]">
                                 <div className="w-full flex items-center justify-center">
-                                    <button className="bg-white px-4 py-1 font-semibold rounded-md w-full text-gray-700">
+                                    <button className="bg-gray-700 px-4 py-1 font-semibold rounded-md w-full text-white">
                                         <Link to="/signin">Login</Link>
                                     </button>
-                                    <button className="bg-gray-700 px-4 py-1 font-semibold rounded-md w-full text-white">
+                                    <button className="bg-white px-4 py-1 font-semibold rounded-md w-full text-gray-700">
                                         <Link to="/signup">Signup</Link>
                                     </button>
                                 </div>
                             </li>
                         ) : (
-                            <li className="relative bottom-4 w-full mt-8">
+                            <li className="absolute bottom-4 w-[90%]">
                                 <div className="w-full flex items-center justify-center">
-                                    <button className="bg-white px-4 py-1 font-semibold rounded-md w-full text-gray-700">
+                                    <button className="bg-gray-700 px-4 py-1 font-semibold rounded-md w-full text-white">
                                         <Link to="/user/profile">Profile</Link>
                                     </button>
-                                    <button className="bg-gray-700 px-4 py-1 font-semibold rounded-md w-full text-white">
+                                    <button className="bg-white px-4 py-1 font-semibold rounded-md w-full text-gray-700">
                                         <Link onClick={onLogout}>Logout</Link>
                                     </button>
                                 </div>
@@ -107,3 +117,4 @@ function HomeLayout({ children }){
 }
 
 export default HomeLayout;
+
